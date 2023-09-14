3925f54f673714b26d9bea34e9fd7fdfdee7b3ed
Rômulo Nunes <romulotg21@gmail.com>
2023-09-14 12:56:34 -0300

    feat: some convert types

---
+++ convert_types.py
@@ -0,0 +1,13 @@
+print(int(10.0))
+print(float(10))
+
+print(int("10"))
+print(float("10"))
+
+value = 10
+value_str = str(value)
+print(type(value))
+print(type(value_str))
+
+print(100 / 2)
+print(100 // 2)
