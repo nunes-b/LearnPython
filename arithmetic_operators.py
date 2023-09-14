cc1eae3c65819c2afdcaf07391eb859a48745362
Rômulo Nunes <romulotg21@gmail.com>
2023-09-14 14:13:47 -0300

    feat: arithmetic_operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,18 @@
+product_1 = 20
+product_2 = 10
+
+print(product_1 + product_2)
+print(product_1 - product_2)
+print(product_1 / product_2)
+print(product_1 // product_2)
+print(product_1 * product_2)
+print(product_1 % product_2)
+print(product_1 ** product_2)
+
+y = 10 / 2 * 30 - 40 + 90 ** 35
+print(" y =", y)
+
+x = 10 + 15 * 4
+print(x)
+x = (10 + 15) * 4
+print(x)
