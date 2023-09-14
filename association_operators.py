491d328fc2cc8192058c2417fe6e1aa2b4c278e3
Rômulo Nunes <romulotg21@gmail.com>
2023-09-14 16:07:09 -0300

    association_operators

---
+++ association_operators.py
@@ -0,0 +1,15 @@
+fruits = ["Apple", "Grape", "Mango", "Banana"]
+print("Orange" in fruits)
+
+fruits = ["Apple", "Grape", "Mango", "Banana", "Orange"]
+print("Orange" in fruits)
+print("orange" in fruits)  # Case sensitive
+
+
+fruits = ["Apple", "Grape", "Mango", "Banana", "Orange", "Lemon"]
+print("Lemon" not in fruits)
+
+
+learn = "Learning Python"
+print("Python" in learn)
+print("Python" not in learn)
