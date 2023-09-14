a48e023ef4bc29ee7eae2306b2dc347ea865acb8
Rômulo Nunes <romulotg21@gmail.com>
2023-09-14 15:51:11 -0300

    id_operators

---
+++ id_operators.py
@@ -0,0 +1,9 @@
+balance = 1000
+limit = 500
+
+print(balance is limit)
+print(balance is not limit)
+print("==================================================")
+balance = limit
+print(balance is limit)
+print(balance is not limit)
